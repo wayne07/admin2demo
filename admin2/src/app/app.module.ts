90801968130a7190c90d6248ec50376fa7a80b2b
juergen seliger <juergen.seliger@gmail.com>
2017-08-02 17:05:14 +0200

    Modul als Kopie der Schulung angelegt

---
+++ admin2/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './home/welcome.component';
+
+@NgModule({
+  imports: [ BrowserModule,
+             HttpModule,
+             ProductModule,
+             AppRoutingModule
+           ],
+  declarations: [ AppComponent, WelcomeComponent ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
